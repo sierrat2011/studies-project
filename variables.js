e0ece80536de3c772cb90ab4d72a583368a87df8
sierrat2011 <46582207+sierrat2011@users.noreply.github.com>
2025-11-21 20:55:50 +0000

    some of variables

--- variables.js
+++ variables.js
@@ -1,6 +1,19 @@
 /**
  * VARIABLES:
  * 
- * 0: (provide general description of the concept)
+ * 0: Variables are named identifiers bound to a section of code called a scope. Variables are a way to create and store values that we can use throughout the application. 
+ * Unlike constants, variables can be reassigned to another value. Variables can hold many datatypes, including number
+ * 1: There are two steps to creating and using variables: declaration and assignment 
  * 
  */
+
+// 1. Declaration //
+// To declare a variable, you must type the word var, followed by your variable name in camelCase. 
+// Unassigned variable will result in undefined until they are assigned
+var myName;
+console.log(myName); // prints 'undefined' to the console
+
+// 2. Assignment or Initialization
+// To assign a variable after it is declared, you must type the variable name, followed by the = symbol, followed by whatever value you want. 
+// This can be done on the same line
+ myName = 'Sierra'
