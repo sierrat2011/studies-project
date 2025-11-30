5044fc5c7df33aee2fb3814035861efc582a3af4
sierrat2011 <46582207+sierrat2011@users.noreply.github.com>
2025-11-30 07:13:19 +0000

    variables done

--- variables.js
+++ variables.js
@@ -1,8 +1,10 @@
 /**
  * VARIABLES:
  * 
- * 0: Variables are named identifiers bound to a section of code called a scope. Variables are a way to create and store values that we can use throughout the application. 
+ * 0: Variables are named identifiers bound to a section of code called a scope. 
+ * Variables are a way to create and store values that we can use throughout the application. 
  * Unlike constants, variables can be reassigned to another value. Variables can hold many datatypes, including number
+ * 
  * 1: There are two steps to creating and using variables: declaration and assignment 
  * 
  */
@@ -14,7 +16,8 @@ var myName;
 console.log(myName); // prints 'undefined' to the console
 
 // 2. Assignment or Initialization //
-// To assign a variable after it is declared, you must type the variable name, followed by the = symbol, followed by whatever value you want. 
+// To assign a variable after it is declared, you must type the variable name, 
+// followed by the = symbol, followed by whatever value you want. 
 // This can be done on the same line
  myName = 'Sierra'
  console.log(myName); // prints 'Sierra' to the console
@@ -25,4 +28,29 @@ console.log(myName); // prints 'undefined' to the console
  console.log(myName); // prints 'Cierra' to the console
 
  // 4. var, let and const //
- // 
+ // Variables can be created and declared with three different keywords: var, let and const
+
+ // var declarative keywork
+ // - can be initialized without a value
+ var name; // variable declared but not given a value
+ // - can be reassigned and re-declared
+ // - can only global or function scoped
+
+ // let declarative keyword
+ // very flexible and is the closest to var
+ // - can be initialized without a value
+ let name; // variable declared but not given a value
+ // - can be reassigned
+ // - are block scoped
+
+ // const declarative keywork
+ // less flexible than var and let
+ // - cannot be initialized without a value
+ // - cannot be reassigned
+ // - are block scoped
+ // const allows you to create variables that are very secure
+
+ // contemporary 
+
+// Hoisting // 
+// var variable are always "hoisted" out of their scope into the global scope. let and const are not hoisted
